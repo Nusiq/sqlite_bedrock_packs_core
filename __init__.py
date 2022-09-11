c3a7647dede6416d0ffd64a49fa58051d6f82a67
Nusiq <34062025+Nusiq@users.noreply.github.com>
2022-09-11 18:57:21 +0200

    Version 0.0.5. Added missing import in __init__

--- __init__.py
+++ __init__.py
@@ -1,5 +1,4 @@
-from .db_main import create_db
-from .db_main import load_rp
+from .db_main import create_db, load_rp, open_db
 
-VERSION = (0, 0, 4)  # COMPATIBILITY BREAK, NEW FEATURE, BUGFIX
+VERSION = (0, 0, 5)  # COMPATIBILITY BREAK, NEW FEATURE, BUGFIX
 __version__ = '.'.join([str(x) for x in VERSION])
